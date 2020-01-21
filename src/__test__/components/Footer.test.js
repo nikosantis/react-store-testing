a3fe13f1fb67378269fd180e22204bdd634176dc
Nikolas Santis <nikosantis@gmail.com>
2020-01-20 22:48:44 -0300

    creating mocks

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -3,8 +3,12 @@ import { mount } from 'enzyme';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
+  const footer = mount(<Footer />);
+
   test('Render of the Footer component', () => {
-    const footer = mount(<Footer />);
     expect(footer.length).toEqual(1);
+  });
+  test('Render of title', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('React Store');
   });
 });
